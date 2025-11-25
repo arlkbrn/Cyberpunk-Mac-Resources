61479d68fee8eec007601221830a498bbfbdebd5
Ariel <210614586+arlkbrn@users.noreply.github.com>
2025-11-25 00:59:53 -0400

    Add more logging addresses

--- Addresses/Misc/Addresses.hpp
+++ Addresses/Misc/Addresses.hpp
@@ -12,12 +12,17 @@ namespace CMR::Addresses
 constexpr uintptr_t ImageBase = 0x100000000;
 
 #pragma region CScript
+constexpr uintptr_t CScript_FTLog = 0x1020946a4 - ImageBase;
+constexpr uintptr_t CScript_FTLogError = 0x102094784 - ImageBase;
+constexpr uintptr_t CScript_FTLogWarning = 0x102094864 - ImageBase;
+
 constexpr uintptr_t CScript_Log = 0x1022111f8 - ImageBase;
 constexpr uintptr_t CScript_LogError = 0x1022112d8 - ImageBase;
-constexpr uintptr_t CScript_LogWarning = 0x1022113b8 - ImageBase; 
-constexpr uintptr_t CScript_LogChannel = 0x102211498 - ImageBase; 
-constexpr uintptr_t CScript_LogChannelWarning = 0x1022116c8 - ImageBase; 
-constexpr uintptr_t CScript_ToStringDEBUG = 0x102b79d10 - ImageBase;
+constexpr uintptr_t CScript_LogWarning = 0x1022113b8 - ImageBase;
+
+constexpr uintptr_t CScript_LogChannel = 0x102211498 - ImageBase;
+constexpr uintptr_t CScript_LogChannelError = 0x1022115b0 - ImageBase;
+constexpr uintptr_t CScript_LogChannelWarning = 0x1022116c8 - ImageBase;
 #pragma endregion
 
 } // namespace CMR::Addresses
